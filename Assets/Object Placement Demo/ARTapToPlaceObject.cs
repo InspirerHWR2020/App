77f97f9989458874970b00ccf43e836c1769b6a0
Lucas Hardt <Luc1412.lh@gmail.com>
2022-10-17 01:18:00 +0200

    Implement Basic Object placement

---
+++ Assets/Object Placement Demo/ARTapToPlaceObject.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    public GameObject objectToPlace; 
+    public GameObject placementIndicator;
+    
+    private ARRaycastManager arRaycastManager;
+    private Pose placementPose;
+    private bool placementPoseIsValid;
+    
+
+    void Start()
+    {
+        arRaycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+
+        if (placementPoseIsValid && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            PlaceObject();
+        }
+    }
+
+    void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        arRaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+        
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+        }
+    }
+
+    void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else placementIndicator.SetActive(false);
+    }
+    
+    void PlaceObject()
+    {
+       Instantiate(objectToPlace, placementPose.position, placementPose.rotation);
+    }
+}
